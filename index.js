b2eb473efdb343969363ca9a05ba1930519c3eb0
Brian <brian@Hannahs-MacBook-Pro-4.local>
2020-09-24 19:09:20 -0400

    reworked ReadMe

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const BuildProfile = require("./buildProfile");
 const inquirer = require("inquirer");
 
-///////////////////////////////////////////////////////////
-/*
-Initial Install:
-1. Run npm install
-2. Go to node_modules/convert-html-to-pdf/lib/index.js
-3. Change Line 4 from exports.default to module.exports
-
-
-*/
-//////////////////////////////////////////////////////////
-
 function promptUser(){
     return inquirer.prompt([
         {
